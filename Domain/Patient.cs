9d3c512b1e9009815e92523d7c1a12b9cda46d7b
matheusgomes062 <matheusgomes062@gmail.com>
2021-05-26 19:52:33 -0300

    first commit

---
+++ Domain/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Cpf { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public bool Comorbidity { get; set; }
+
+        public string Address { get; set; }
+
+        public string District { get; set; }
+
+        public string State { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# VaccineAPI6
