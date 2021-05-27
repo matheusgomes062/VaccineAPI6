2f888026ad19186a6ef7d2b4a648b6b449cd29d5
matheusgomes062 <matheusgomes062@gmail.com>
2021-05-27 02:12:00 -0300

    feat: added create handler

--- API/Controllers/PatientsController.cs
+++ API/Controllers/PatientsController.cs
@@ -8,18 +8,24 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
   public class PatientsController : BaseApiController
+  {
+
+    [HttpGet]
+    public async Task<ActionResult<List<Patient>>> GetPatients()
     {
-
-        [HttpGet]
-        public async Task<ActionResult<List<Patient>>> GetPatients()
-        {
-            return await Mediator.Send(new List.Query());
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Patient>> GetPatient(Guid id)
-        {
-            return await Mediator.Send(new Details.Query{Id = id});
-        }
+      return await Mediator.Send(new List.Query());
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Patient>> GetPatient(Guid id)
+    {
+      return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreatePatient(Patient patient)
+    {
+      return Ok(await Mediator.Send(new Create.Command { Patient = patient }));
+    }
+  }
 }
